50f93251b276e85b79f50315b630845e5087c264
Arun <arun.rs@identitymine.com>
2015-12-31 11:34:45 +0530

    Initial code drop

---
+++ WPF Clock/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WPF_Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 1);
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            time.Text = DateTime.Now.ToLongTimeString();
+        }
+        private void pathListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            List<int> Items = new List<int>();
+            for (int i = 0; i < 60; i++)
+            {
+                Items.Add(i);
+            }
+            pathListBox.ItemsSource = Items;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            time.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
